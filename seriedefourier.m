f2e4bc54de36d970a3eadb3a6f3f2a878f88a8ef
bruunoporto0 <bruunoporto0@gmail.com>
2022-01-08 22:29:40 -0300

    added new file wich contains the formula to get the square function of the harmonic sinal, created new variable named xt_final

---
+++ seriedefourier.m
@@ -0,0 +1,10 @@
+F_A = 440;
+Fs=8000;
+Ts=1/Fs;
+t=[0:Ts:1];
+pi = 3.14
+h=1    % alterar o valor de 1 até 321 que é o ultimo valor pedido
+xt_harmonic = 4/pi*(sin(2*pi*f*t));
+for i = 3:2:1000
+  xt_harmonic = xt_harmonic + (4/pi*(1/i*sin(i*2*pi*F_A*h*t)))
+endfor
--- trabalho3sinais1.m
+++ trabalho3sinais1.m
@@ -1,13 +1,10 @@
-F_A = 440;
 Fs=8000;
 Ts=1/Fs;
 t=[0:Ts:1];
 pi=3.14;
-A=sin(2*pi*3960*t)
-sound(xt,Fs);%440
-
+sound(xt,Fs);%440 frequencia fundamental
 sound(xt_oitava,Fs);%880
 sound(xt_quinta,Fs);%1320
 sound(xt_quarta,Fs); %1760
 sound(xt_segunda,Fs);%3960
-sound(A,Fs)
+sound(xt_final,Fs);%141240
